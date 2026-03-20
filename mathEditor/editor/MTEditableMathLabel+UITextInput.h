57c8d0fce2c8146f5b58a93c4cbe13a9ba1c62bc
Madiyar Aitbayev <maitbayev@snapchat.com>
2026-03-20 12:03:34 +0000

    Isolate UITextInput methods

---
+++ mathEditor/editor/MTEditableMathLabel+UITextInput.h
@@ -0,0 +1,17 @@
+//
+//  NSObject+MTEditableMathLabel_UITextInput.h
+//  MathEditor
+//
+//  Created by Madiyar Aitbayev on 20/03/2026.
+//
+
+#import <Foundation/Foundation.h>
+#import  "MTEditableMathLabel.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTEditableMathLabel ()
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ mathEditor/editor/NSObject+MTEditableMathLabel_UITextInput.m
@@ -0,0 +1,12 @@
+//
+//  NSObject+MTEditableMathLabel_UITextInput.m
+//  MathEditor
+//
+//  Created by Madiyar Aitbayev on 20/03/2026.
+//
+
+#import "NSObject+MTEditableMathLabel_UITextInput.h"
+
+@implementation NSObject (MTEditableMathLabel_UITextInput)
+
+@end
