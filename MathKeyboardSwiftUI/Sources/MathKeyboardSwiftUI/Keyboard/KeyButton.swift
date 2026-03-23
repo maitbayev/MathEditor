67a8bcae2a3df099a31a2a8f5debd7f4f5a9723f
Madiyar Aitbayev <maitbayev@snapchat.com>
2026-03-23 20:36:46 +0000

    Refactor

---
+++ MathKeyboardSwiftUI/Sources/MathKeyboardSwiftUI/Keyboard/KeyButton.swift
@@ -0,0 +1,73 @@
+//
+//  KeyButton.swift
+//  MathKeyboardSwiftUI
+//
+//  Created by Madiyar Aitbayev on 23/03/2026.
+//
+
+import SwiftUI
+
+struct KeyButton: View {
+  var cell: KeyboardCell
+
+  init(_ cell: KeyboardCell) {
+    self.cell = cell
+  }
+
+  var body: some View {
+    Button(action: cell.action) {
+      ZStack {
+        Rectangle().fill(Color.white.opacity(0.001))
+        if let overlayAsset = cell.overlayAsset {
+          mtMathImage(overlayAsset)
+            .resizable()
+        }
+        switch cell.content {
+        case .text(let text):
+          Text(text.value)
+            .font(.custom(text.fontName, size: text.fontSize))
+            .foregroundColor(textColor(for: text.tone))
+        case .image(let image):
+          mtMathImage(image.name)
+            .renderingMode(.original)
+            .resizable()
+            .scaledToFit()
+            .padding(image.padding)
+        }
+      }
+    }
+    .buttonStyle(
+      KeyboardPressStyle(pressedAsset: cell.pressedAsset)
+    )
+    .disabled(!cell.enabled)
+    .opacity(cell.enabled ? 1 : 0.75)
+    .accessibilityLabel(cell.accessibilityLabel)
+  }
+}
+
+private func textColor(for tone: KeyboardCell.TextTone) -> Color {
+  switch tone {
+  case .light: .white
+  case .dark: .black
+  case .disabled: Color(white: 0.67)
+  }
+}
+
+private struct KeyboardPressStyle: ButtonStyle {
+  let pressedAsset: String?
+
+  func makeBody(configuration: Configuration) -> some View {
+    ZStack {
+      if configuration.isPressed {
+        if let pressedAsset {
+          mtMathImage(pressedAsset)
+            .resizable()
+            .opacity(1.0)
+        } else {
+          Color.clear
+        }
+      }
+      configuration.label
+    }
+  }
+}
