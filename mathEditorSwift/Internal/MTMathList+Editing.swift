1527558506c37db2ae3fcfb721a51db0741b054a
Madiyar <Madiyar92@gmail.com>
2026-03-24 14:30:09 +0000

    Fix MTMathList editing Swift API compatibility

--- mathEditorSwift/Internal/MTMathList+Editing.swift
+++ mathEditorSwift/Internal/MTMathList+Editing.swift
@@ -11,7 +11,7 @@ import iosMath
 extension MTMathList {
   @objc(insertAtom:atListIndex:)
   public func insert(_ atom: MTMathAtom, atListIndex index: MTMathListIndex) {
-    if index.atomIndex > atoms.count {
+    if index.atomIndex > UInt(atoms.count) {
       let exception = NSException(
         name: .rangeException,
         reason: "Index \(index.atomIndex) is out of bounds for list of size \(atoms.count)",
@@ -22,53 +22,66 @@ extension MTMathList {
     }
 
     switch index.subIndexType {
-    case .none:
+    case .subIndexTypeNone:
       insert(atom, at: index.atomIndex)
 
-    case .nucleus:
-      guard let currentAtom = atoms[index.atomIndex] as? MTMathAtom else { return }
+    case .subIndexTypeNucleus:
+      let atomIndex = Int(index.atomIndex)
+      guard let currentAtom = atoms[atomIndex] as? MTMathAtom else { return }
       assert(currentAtom.subScript != nil || currentAtom.superScript != nil,
              "Nuclear fusion is not supported if there are no subscripts or superscripts.")
       assert(atom.subScript == nil && atom.superScript == nil,
              "Cannot fuse with an atom that already has a subscript or a superscript")
+      guard let subIndex = index.sub else { return }
 
       atom.subScript = currentAtom.subScript
       atom.superScript = currentAtom.superScript
       currentAtom.subScript = nil
       currentAtom.superScript = nil
-      insert(atom, at: index.atomIndex + index.subIndex.atomIndex)
+      insert(atom, at: index.atomIndex + subIndex.atomIndex)
 
-    case .degree, .radicand:
-      guard let radical = atoms[index.atomIndex] as? MTRadical, radical.type == .radical else {
+    case .subIndexTypeDegree, .subIndexTypeRadicand:
+      let atomIndex = Int(index.atomIndex)
+      guard let radical = atoms[atomIndex] as? MTRadical, radical.type == .radical else {
         assertionFailure("No radical found at index \(index.atomIndex)")
         return
       }
-      if index.subIndexType == .degree {
-        radical.degree.insert(atom, atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return }
+      if index.subIndexType == .subIndexTypeDegree {
+        radical.degree.insert(atom, atListIndex: subIndex)
       } else {
-        radical.radicand.insert(atom, atListIndex: index.subIndex)
+        radical.radicand.insert(atom, atListIndex: subIndex)
       }
 
-    case .denominator, .numerator:
-      guard let frac = atoms[index.atomIndex] as? MTFraction, frac.type == .fraction else {
+    case .subIndexTypeDenominator, .subIndexTypeNumerator:
+      let atomIndex = Int(index.atomIndex)
+      guard let frac = atoms[atomIndex] as? MTFraction, frac.type == .fraction else {
         assertionFailure("No fraction found at index \(index.atomIndex)")
         return
       }
-      if index.subIndexType == .numerator {
-        frac.numerator.insert(atom, atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return }
+      if index.subIndexType == .subIndexTypeNumerator {
+        frac.numerator.insert(atom, atListIndex: subIndex)
       } else {
-        frac.denominator.insert(atom, atListIndex: index.subIndex)
+        frac.denominator.insert(atom, atListIndex: subIndex)
       }
 
-    case .subscript:
-      guard let current = atoms[index.atomIndex] as? MTMathAtom else { return }
+    case .subIndexTypeSubscript:
+      let atomIndex = Int(index.atomIndex)
+      guard let current = atoms[atomIndex] as? MTMathAtom else { return }
       assert(current.subScript != nil, "No subscript for atom at index \(index.atomIndex)")
-      current.subScript?.insert(atom, atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return }
+      current.subScript?.insert(atom, atListIndex: subIndex)
 
-    case .superscript:
-      guard let current = atoms[index.atomIndex] as? MTMathAtom else { return }
+    case .subIndexTypeSuperscript:
+      let atomIndex = Int(index.atomIndex)
+      guard let current = atoms[atomIndex] as? MTMathAtom else { return }
       assert(current.superScript != nil, "No superscript for atom at index \(index.atomIndex)")
-      current.superScript?.insert(atom, atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return }
+      current.superScript?.insert(atom, atListIndex: subIndex)
+
+    case .subIndexTypeInner:
+      break
 
     @unknown default:
       break
@@ -77,7 +90,7 @@ extension MTMathList {
 
   @objc(removeAtomAtListIndex:)
   public func removeAtom(atListIndex index: MTMathListIndex) {
-    if index.atomIndex >= atoms.count {
+    if index.atomIndex >= UInt(atoms.count) {
       let exception = NSException(
         name: .rangeException,
         reason: "Index \(index.atomIndex) is out of bounds for list of size \(atoms.count)",
@@ -88,16 +101,17 @@ extension MTMathList {
     }
 
     switch index.subIndexType {
-    case .none:
+    case .subIndexTypeNone:
       removeAtom(at: index.atomIndex)
 
-    case .nucleus:
-      guard let currentAtom = atoms[index.atomIndex] as? MTMathAtom else { return }
+    case .subIndexTypeNucleus:
+      let atomIndex = Int(index.atomIndex)
+      guard let currentAtom = atoms[atomIndex] as? MTMathAtom else { return }
       assert(currentAtom.subScript != nil || currentAtom.superScript != nil,
              "Nuclear fission is not supported if there are no subscripts or superscripts.")
       var previous: MTMathAtom?
       if index.atomIndex > 0 {
-        previous = atoms[index.atomIndex - 1] as? MTMathAtom
+        previous = atoms[Int(index.atomIndex - 1)] as? MTMathAtom
       }
       if let previous,
          previous.subScript == nil,
@@ -109,37 +123,48 @@ extension MTMathList {
         currentAtom.nucleus = ""
       }
 
-    case .radicand, .degree:
-      guard let radical = atoms[index.atomIndex] as? MTRadical, radical.type == .radical else {
+    case .subIndexTypeRadicand, .subIndexTypeDegree:
+      let atomIndex = Int(index.atomIndex)
+      guard let radical = atoms[atomIndex] as? MTRadical, radical.type == .radical else {
         assertionFailure("No radical found at index \(index.atomIndex)")
         return
       }
-      if index.subIndexType == .degree {
-        radical.degree.removeAtom(atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return }
+      if index.subIndexType == .subIndexTypeDegree {
+        radical.degree.removeAtom(atListIndex: subIndex)
       } else {
-        radical.radicand.removeAtom(atListIndex: index.subIndex)
+        radical.radicand.removeAtom(atListIndex: subIndex)
       }
 
-    case .denominator, .numerator:
-      guard let frac = atoms[index.atomIndex] as? MTFraction, frac.type == .fraction else {
+    case .subIndexTypeDenominator, .subIndexTypeNumerator:
+      let atomIndex = Int(index.atomIndex)
+      guard let frac = atoms[atomIndex] as? MTFraction, frac.type == .fraction else {
         assertionFailure("No fraction found at index \(index.atomIndex)")
         return
       }
-      if index.subIndexType == .numerator {
-        frac.numerator.removeAtom(atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return }
+      if index.subIndexType == .subIndexTypeNumerator {
+        frac.numerator.removeAtom(atListIndex: subIndex)
       } else {
-        frac.denominator.removeAtom(atListIndex: index.subIndex)
+        frac.denominator.removeAtom(atListIndex: subIndex)
       }
 
-    case .subscript:
-      guard let current = atoms[index.atomIndex] as? MTMathAtom else { return }
+    case .subIndexTypeSubscript:
+      let atomIndex = Int(index.atomIndex)
+      guard let current = atoms[atomIndex] as? MTMathAtom else { return }
       assert(current.subScript != nil, "No subscript for atom at index \(index.atomIndex)")
-      current.subScript?.removeAtom(atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return }
+      current.subScript?.removeAtom(atListIndex: subIndex)
 
-    case .superscript:
-      guard let current = atoms[index.atomIndex] as? MTMathAtom else { return }
+    case .subIndexTypeSuperscript:
+      let atomIndex = Int(index.atomIndex)
+      guard let current = atoms[atomIndex] as? MTMathAtom else { return }
       assert(current.superScript != nil, "No superscript for atom at index \(index.atomIndex)")
-      current.superScript?.removeAtom(atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return }
+      current.superScript?.removeAtom(atListIndex: subIndex)
+
+    case .subIndexTypeInner:
+      break
 
     @unknown default:
       break
@@ -151,43 +176,54 @@ extension MTMathList {
     let start = range.start
 
     switch start.subIndexType {
-    case .none:
-      removeAtoms(in: NSRange(location: start.atomIndex, length: range.length))
+    case .subIndexTypeNone:
+      removeAtoms(in: NSRange(location: Int(start.atomIndex), length: Int(range.length)))
 
-    case .nucleus:
+    case .subIndexTypeNucleus:
       assertionFailure("Nuclear fission is not supported")
 
-    case .radicand, .degree:
-      guard let radical = atoms[start.atomIndex] as? MTRadical, radical.type == .radical else {
+    case .subIndexTypeRadicand, .subIndexTypeDegree:
+      let atomIndex = Int(start.atomIndex)
+      guard let radical = atoms[atomIndex] as? MTRadical, radical.type == .radical else {
         assertionFailure("No radical found at index \(start.atomIndex)")
         return
       }
-      if start.subIndexType == .degree {
-        radical.degree.removeAtoms(inListIndexRange: range.subIndexRange)
+      guard let subIndexRange = range.subIndex() else { return }
+      if start.subIndexType == .subIndexTypeDegree {
+        radical.degree.removeAtoms(inListIndexRange: subIndexRange)
       } else {
-        radical.radicand.removeAtoms(inListIndexRange: range.subIndexRange)
+        radical.radicand.removeAtoms(inListIndexRange: subIndexRange)
       }
 
-    case .denominator, .numerator:
-      guard let frac = atoms[start.atomIndex] as? MTFraction, frac.type == .fraction else {
+    case .subIndexTypeDenominator, .subIndexTypeNumerator:
+      let atomIndex = Int(start.atomIndex)
+      guard let frac = atoms[atomIndex] as? MTFraction, frac.type == .fraction else {
         assertionFailure("No fraction found at index \(start.atomIndex)")
         return
       }
-      if start.subIndexType == .numerator {
-        frac.numerator.removeAtoms(inListIndexRange: range.subIndexRange)
+      guard let subIndexRange = range.subIndex() else { return }
+      if start.subIndexType == .subIndexTypeNumerator {
+        frac.numerator.removeAtoms(inListIndexRange: subIndexRange)
       } else {
-        frac.denominator.removeAtoms(inListIndexRange: range.subIndexRange)
+        frac.denominator.removeAtoms(inListIndexRange: subIndexRange)
       }
 
-    case .subscript:
-      guard let current = atoms[start.atomIndex] as? MTMathAtom else { return }
+    case .subIndexTypeSubscript:
+      let atomIndex = Int(start.atomIndex)
+      guard let current = atoms[atomIndex] as? MTMathAtom else { return }
       assert(current.subScript != nil, "No subscript for atom at index \(start.atomIndex)")
-      current.subScript?.removeAtoms(inListIndexRange: range.subIndexRange)
+      guard let subIndexRange = range.subIndex() else { return }
+      current.subScript?.removeAtoms(inListIndexRange: subIndexRange)
 
-    case .superscript:
-      guard let current = atoms[start.atomIndex] as? MTMathAtom else { return }
+    case .subIndexTypeSuperscript:
+      let atomIndex = Int(start.atomIndex)
+      guard let current = atoms[atomIndex] as? MTMathAtom else { return }
       assert(current.superScript != nil, "No superscript for atom at index \(start.atomIndex)")
-      current.superScript?.removeAtoms(inListIndexRange: range.subIndexRange)
+      guard let subIndexRange = range.subIndex() else { return }
+      current.superScript?.removeAtoms(inListIndexRange: subIndexRange)
+
+    case .subIndexTypeInner:
+      break
 
     @unknown default:
       break
@@ -197,38 +233,45 @@ extension MTMathList {
   @objc(atomAtListIndex:)
   public func atom(atListIndex index: MTMathListIndex?) -> MTMathAtom? {
     guard let index else { return nil }
-    guard index.atomIndex < atoms.count else { return nil }
-    guard let atom = atoms[index.atomIndex] as? MTMathAtom else { return nil }
+    guard index.atomIndex < UInt(atoms.count) else { return nil }
+    guard let atom = atoms[Int(index.atomIndex)] as? MTMathAtom else { return nil }
 
     switch index.subIndexType {
-    case .none, .nucleus:
+    case .subIndexTypeNone, .subIndexTypeNucleus:
       return atom
 
-    case .subscript:
-      return atom.subScript?.atom(atListIndex: index.subIndex)
+    case .subIndexTypeSubscript:
+      guard let subIndex = index.sub else { return nil }
+      return atom.subScript?.atom(atListIndex: subIndex)
 
-    case .superscript:
-      return atom.superScript?.atom(atListIndex: index.subIndex)
+    case .subIndexTypeSuperscript:
+      guard let subIndex = index.sub else { return nil }
+      return atom.superScript?.atom(atListIndex: subIndex)
 
-    case .radicand, .degree:
+    case .subIndexTypeRadicand, .subIndexTypeDegree:
       guard let radical = atom as? MTRadical, atom.type == .radical else {
         return nil
       }
-      if index.subIndexType == .degree {
-        return radical.degree.atom(atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return nil }
+      if index.subIndexType == .subIndexTypeDegree {
+        return radical.degree.atom(atListIndex: subIndex)
       } else {
-        return radical.radicand.atom(atListIndex: index.subIndex)
+        return radical.radicand.atom(atListIndex: subIndex)
       }
 
-    case .numerator, .denominator:
+    case .subIndexTypeNumerator, .subIndexTypeDenominator:
       guard let frac = atom as? MTFraction, atom.type == .fraction else {
         return nil
       }
-      if index.subIndexType == .denominator {
-        return frac.denominator.atom(atListIndex: index.subIndex)
+      guard let subIndex = index.sub else { return nil }
+      if index.subIndexType == .subIndexTypeDenominator {
+        return frac.denominator.atom(atListIndex: subIndex)
       } else {
-        return frac.numerator.atom(atListIndex: index.subIndex)
+        return frac.numerator.atom(atListIndex: subIndex)
       }
+
+    case .subIndexTypeInner:
+      return nil
 
     @unknown default:
       return nil
