a11415268681db590f93207a5203b04ff51be817
Madiyar Aitbayev <maitbayev@snapchat.com>
2026-03-26 13:02:59 +0000

    iosMath simpliufy

--- MathEditorSwift/Package.swift
+++ MathEditorSwift/Package.swift
@@ -17,7 +17,7 @@ let package = Package(
   targets: [
     .target(
       name: "MathEditorSwift",
-      dependencies: [.product(name: "iosMath", package: "iosMath")]
+      dependencies: ["iosMath"]
     )
   ]
 )
